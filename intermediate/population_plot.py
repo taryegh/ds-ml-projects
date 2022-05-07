a61e93f1815a611a0a8ec6817f446b0f3766038e
Taron Yeghoyan <49352957+taryegh@users.noreply.github.com>
2022-05-07 11:43:44 +0400

    Update population_plot.py

--- intermediate/population_plot.py
+++ intermediate/population_plot.py
@@ -1,4 +1,6 @@
 import matplotlib.pyplot as plt
+import numpy
+
 year = [1950,
  1951,
  1952,
@@ -301,6 +303,17 @@ pop = [2.53,
  10.83,
  10.84,
  10.85]
+
+# Add more data
+year = [1800, 1850, 1900] + year
+pop = [1.0, 1.262, 1.650] + pop
+
 plt.plot(year, pop)
-plt.scatter(year, pop)
+
+plt.xlabel('Year')
+plt.ylabel('Population')
+plt.title('World Population Projections')
+plt.yticks([0, 2, 4, 6, 8, 10],
+           ['0B', '2B', '4B', '6B', '8B', '10B'])
+
 plt.show()
